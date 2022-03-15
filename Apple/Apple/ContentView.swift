baadc063927c5b903c4fe54b2273506d3f6f177c
غربي الشمري <100927646+garbyalshammari@users.noreply.github.com>
2022-03-15 23:22:14 +0300

    efdeg

--- Apple/Apple/ContentView.swift
+++ Apple/Apple/ContentView.swift
@@ -7,7 +7,8 @@
 
 import SwiftUI
 
-struct contentView: View {
+struct ContentView: View {
+    
     //vars
     var ipadAir = 220
     var airPodsMax = 139.9
@@ -15,12 +16,13 @@ struct contentView: View {
     var appleTV = 89.9
     var airTag = 10.9
     //states
-    @State var a1 = 0
-    @State var a2 = 0
-    @State var a3 = 0
-    @State var a4 = 0
-    @State var a5 = 0
+    @State var a1 = 0.0
+    @State var a2 = 0.0
+    @State var a3 = 0.0
+    @State var a4 = 0.0
+    @State var a5 = 0.0
     @State var amount = 0.0
+    @State var money = ""
     var body : some View {
         VStack {
             
@@ -38,6 +40,90 @@ struct contentView: View {
             Text("Products")
                 .font(.title)
                 .fontWeight(.regular)
+            HStack{
+            Stepper("\(a1)", value: $a1,in: 0...12)
+                Text("220 kd")
+                Spacer()
+                Text("ipadAir")
+                    .font(.headline)
+                    .fontWeight(.medium)
+                Image("a1")
+                    .resizable()
+                    .frame(width: 80, height: 80)
+                    .scaledToFit()
+                    }
+            HStack{
+            Stepper("\(a2)", value: $a1,in: 0...12)
+                Text("139.9 kd")
+                Spacer()
+                Text("air PodsMax")
+                    .font(.headline)
+                    .fontWeight(.medium)
+                Image("a2")
+                    .resizable()
+                    .frame(width: 80, height: 80)
+                    .scaledToFit()
+                    }
+            HStack{
+            Stepper("\(a3)", value: $a1,in: 0...12)
+                Text("117.9 kd")
+                Spacer()
+                Text("apple Watch")
+                    .font(.headline)
+                    .fontWeight(.medium)
+                Image("a3")
+                    .resizable()
+                    .frame(width: 80, height: 80)
+                    .scaledToFit()
+                    }
+            HStack{
+            Stepper("\(a4)", value: $a1,in: 0...12)
+                Text("89.9 kd")
+                Spacer()
+                Text("apple TV")
+                    .font(.headline)
+                    .fontWeight(.medium)
+                Image("a4")
+                    .resizable()
+                    .frame(width: 80, height: 80)
+                    .scaledToFit()
+                    }
+            HStack{
+            Stepper("\(a5)", value: $a1,in: 0...12)
+                Text("10.9 kd")
+                Spacer()
+                Text("AirTag")
+                    .font(.headline)
+                    .fontWeight(.medium)
+                Image("a5")
+                    .resizable()
+                    .frame(width: 80, height: 80)
+                    .scaledToFit()
+                    }
+            Group{
+                Text("المبلغ الذي لديك")
+                Text("اضغط لمعرفة الفاتورة")
+                    .frame(width: 200, height: 60)
+                    .background(Color.black)
+                    .cornerRadius(9)
+                    .onTapGesture {
+                        
+                     let p1 = ipadAir * Double(a1)
+                    let p2 = airPodsMax * Double(a2)
+                     let p3 = appleWATCH * Double(a3)
+                    let p4 = appleTV * Double(a4)
+                     let p5 = airTag * Double(a5)
+                    amount = p1 + p2 + p3 + p4 + p5
+                        }
+                Text("المبلغ الاجمالي:\(amount,specifier: "%.2f")")
+                TextField("المبلغ", text: $money)
+                
+                
+                
+                
+            }
+            
+            }
             
             
             
@@ -64,7 +150,7 @@ struct contentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        contentView()
+        ContentView()
     }
 }
-}
+
